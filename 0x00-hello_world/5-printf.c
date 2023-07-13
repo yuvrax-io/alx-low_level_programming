282f61c34408acad274c86eb39ad283539bc30d0
yousri rachid <yuvrax.io@gmail.com>
2023-07-13 08:07:32 -0700

    modify

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main -  prints exactly with proper grammar, but the outcome is a piece of art,
+ * main -  prints exactly with proper grammar
+ * but the outcome is a piece of art,
  * Print exactly the sentence
  * Return: Always 0 (Success)
  */
