551e07555743fab13f390cf189b46e5233bfa339
yousri rachid <yuvrax.io@gmail.com>
2023-07-18 16:51:32 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @x: first number to add
+ * @y: second number to add
+ * Return: returns the result of the addition
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
