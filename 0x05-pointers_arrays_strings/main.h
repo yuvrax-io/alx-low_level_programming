6ed681067bfdf86c35d7524619317959bf19084e
yousri rachid <yuvrax.io@gmail.com>
2023-07-24 05:25:54 -0700

    READme.md

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: file containing prototypes functions
+ *       for project 0x04-pointers_arrays_strings .
+ */
+
+void reset_to_98(int *n);
+
+
+
+
+#endif
