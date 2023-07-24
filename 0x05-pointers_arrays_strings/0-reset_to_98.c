5fc66e6bdcdf92422d678cbdf499ec2ba03ebd14
yousri rachid <yuvrax.io@gmail.com>
2023-07-24 05:30:14 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - that takes a pointer to an int as parameter
+ *      updates the value it points to to 98
+ * @nu: pointer of funtion
+ * Return: void number 98
+ */
+
+void reset_to_98(int *nu)
+{
+	*nu = 998;
+}
