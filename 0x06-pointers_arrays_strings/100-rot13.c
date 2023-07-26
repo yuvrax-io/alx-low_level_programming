a3b64f865a18bf3e80354dc3bbc47a16be16ffb1
yousri rachid <yuvrax.io@gmail.com>
2023-07-26 15:45:49 -0700

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 -  function that encodes a string using rot13
+ * @str: string
+ * Return: return result
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+	char normal[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot13[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; normal[j] != '\0'; j++)
+		{
+			if (s[i] == normal[j])
+			{
+				s[i] = rot13[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
