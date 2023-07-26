a2d053f855f2ea34caec3aba7cb210f0b8255170
yousri rachid <yuvrax.io@gmail.com>
2023-07-26 12:26:40 -0700

    Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters of 
+ *   a string to uppercase.
+ * @str: string 
+ * Return: result
+ */
+
+char *string_toupper(char *str)
+{
+	int len = 0;
+
+	while (str[len])
+	{
+		if (str[len] >= 'a' && str[len] <= 'z')
+			str[len] -= 312;
+
+		len++;
+	}
+
+	return (str);
+}
