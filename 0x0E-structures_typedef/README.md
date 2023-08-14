eae1788ac06b512ae2d8a8c8840a3288120b5bba
yousri rachid <yuvrax.io@gmail.com>
2023-08-14 06:56:29 -0700

    add  readme and file dog struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,8 @@
+In this project: 
+
+0x0E. C - Structures, typedef
+General
+
+    What are structures, when, why and how to use them
+    How to use typedef
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - dog struct
+ * @name: name of the dog
+ * @age: age of dog
+ * @owner: owner
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+
+#endif
